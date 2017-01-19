171a346c4f642038e53000010013aa4f8e1b4967
Scott Mebberson <scott@scottmebberson.com>
2017-01-19 14:37:41 +1030

    First cut of travis integration.

---
+++ vagrant/scripts/test-travis.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+WP="/vagrant"
+
+# Set WP based on $TRAVIS_BUILD_DIR.
+if test ! -z "$TRAVIS_BUILD_DIR"; then
+    WP="$TRAVIS_BUILD_DIR"
+fi
+
+# Make sure RabbitMQ is setup as we need it to be.
+"${WP}/vagrant/scripts/pretest.sh"
+
+DEBUG=lib:logger:* "${WP}/idearium-lib/node_modules/.bin/istanbul" cover "${WP}/idearium-lib/node_modules/.bin/_mocha" -- -R spec
