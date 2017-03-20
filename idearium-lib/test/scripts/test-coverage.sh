27a29e8792e90585f3f5438d6a9c90f36d1aa2e9
Scott Mebberson <scott@scottmebberson.com>
2017-03-21 09:07:27 +1030

    Fixed issues with testing on Travis CI.

--- idearium-lib/test/scripts/test-coverage.sh
+++ idearium-lib/test/scripts/test-coverage.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-WP="."
+WP="/vagrant"
 
 if test ! -z "$TRAVIS_BUILD_DIR"; then
     WP="$TRAVIS_BUILD_DIR"
 fi
 
 # Make sure RabbitMQ is setup as we need it to be.
-../vagrant/scripts/pretest.sh
+"${WP}/idearium-lib/test/scripts/pretest.sh"
 
 # Review code coverage while running the test.
-DEBUG=lib:logger:* "${WP}/node_modules/.bin/istanbul" cover "${WP}/node_modules/.bin/_mocha" -- --trace-deprecation --check-leaks
+DEBUG=lib:logger:* "${WP}/idearium-lib/node_modules/.bin/istanbul" cover "${WP}/idearium-lib/node_modules/.bin/_mocha" -- --trace-deprecation --check-leaks
--- idearium-lib/test/scripts/test-travis.sh
+++ idearium-lib/test/scripts/test-travis.sh
@@ -8,6 +8,6 @@ if test ! -z "$TRAVIS_BUILD_DIR"; then
 fi
 
 # Make sure RabbitMQ is setup as we need it to be.
-"${WP}/vagrant/scripts/pretest.sh"
+"${WP}/idearium-lib/test/scripts/pretest.sh"
 
 DEBUG=lib:logger:* "${WP}/idearium-lib/node_modules/.bin/istanbul" cover "${WP}/idearium-lib/node_modules/.bin/_mocha" -- -R spec
