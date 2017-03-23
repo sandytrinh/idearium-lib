944d7eafa113afbe798b611b484a52b0454b55c8
Scott Mebberson <scott@scottmebberson.com>
2017-03-24 08:29:10 +1030

    Unified the test scripts.

--- idearium-lib/test/scripts/test.sh
+++ idearium-lib/test/scripts/test.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-DEBUG=lib:logger:* MQ_PORT=56722 ./node_modules/.bin/_mocha
+WP="/vagrant"
+
+# Set WP based on $TRAVIS_BUILD_DIR.
+if test ! -z "$TRAVIS_BUILD_DIR"; then
+    WP="$TRAVIS_BUILD_DIR"
+fi
+
+# Set WP based on $SEMAPHORE_PROJECT_DIR.
+if test ! -z "$SEMAPHORE_PROJECT_DIR"; then
+    WP="$SEMAPHORE_PROJECT_DIR"
+fi
+
+# Make sure RabbitMQ is setup as we need it to be.
+"${WP}/idearium-lib/test/scripts/pretest.sh"
+
+DEBUG=lib:logger:* MQ_PORT=56722 "${WP}/idearium-lib/node_modules/.bin/_mocha"
