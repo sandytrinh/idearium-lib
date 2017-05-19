2c286805dd584d4d576785e4569b52f4e1b7b2da
Scott Mebberson <scott@scottmebberson.com>
2017-05-19 11:20:27 +0930

    Updated the example to use the new RPC class.

--- live/config/config.js
+++ live/config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mqUrl: process.env.MQ_URL || 'amqps://idearium:Id3Ar1um@mq.common.idearium.local:5671?heartbeat=5'
+    mqUrl: process.env.MQ_URL || 'amqps://idearium:Id3Ar1um@mq.common.idearium.local:5671?heartbeat=5',
+    logLevel: process.env.LOG_LEVEL || 'debug'
 };
--- live/server.sh
+++ live/server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Start the node client with appropriate NODE_ENV.
-NODE_ENV=development node server.js
+NODE_ENV=development DEBUG=idearium-lib:* node server.js
