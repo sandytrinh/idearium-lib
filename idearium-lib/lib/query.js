e203c5c14229ad664f2e8b7545e6dc0df52ccd11
Allan Chau <allan.chau@icloud.com>
2017-06-08 10:09:59 +0930

    Update to return the query object and add jsdoc examples

--- idearium-lib/lib/query.js
+++ idearium-lib/lib/query.js
@@ -2,7 +2,19 @@
 
 /**
  * Assign some defaults to the passed Mongoose query options.
+ * @example
+ * // returns {
+ *     filter: { _id: id },
+ *     lean: true,
+ *     limit: 10,
+ *     projection: '',
+ * }
+ * getOptions({ filter: { _id: id });
  * @param {Object} options Mongoose query options.
+ * @param {Object} options.filter Mongo query filters.
+ * @param {Boolean} options.lean Whether to return a plain JavaScript object or not.
+ * @param {Number} options.limit Number of documents to return.
+ * @param {String} options.projection Space delimited string containing the fields to return.
  * @return {Object} Returns the options object for Mongoose queries.
  */
 const getOptions = (options) => {
@@ -27,8 +39,19 @@ const getOptions = (options) => {
 
 /**
  * Find documents from a model.
+ * @example
+ * query.find(UserModel, {
+ *     filter: { _id: id },
+ *     lean: false, // Defaults to true
+ *     limit: 5, // Defaults to 10
+ *     projection: 'email username', // _id is always returned
+ * });
  * @param {Object} model Mongoose model.
  * @param {Object} options Mongoose query options.
+ * @param {Object} options.filter Mongo query filters.
+ * @param {Boolean} options.lean Whether to return a plain JavaScript object or not.
+ * @param {Number} options.limit Number of documents to return.
+ * @param {String} options.projection Space delimited string containing the fields to return.
  * @return {Promise} Returns a Mongoose query.
  */
 const find = (model, options) => {
@@ -44,8 +67,17 @@ const find = (model, options) => {
 
 /**
  * Find one document from a model.
+ * @example
+ * query.findOne(UserModel, {
+ *     filter: { _id: id },
+ *     lean: false, // Defaults to true
+ *     projection: 'email username', // _id is always returned
+ * });
  * @param {Object} model Mongoose model.
  * @param {Object} options Mongoose query options.
+ * @param {Object} options.filter Mongo query filters.
+ * @param {Boolean} options.lean Whether to return a plain JavaScript object or not.
+ * @param {String} options.projection Space delimited string containing the fields to return.
  * @return {Promise} Returns a Mongoose query.
  */
 const findOne = (model, options) => {
