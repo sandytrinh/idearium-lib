88b1fcc71d303f02d51205ad6a8c688ce632c06b
Allan Chau <allan.chau@icloud.com>
2017-06-08 12:43:10 +0930

    Update query tests to be more exhaustive

--- idearium-lib/test/query.js
+++ idearium-lib/test/query.js
@@ -16,6 +16,7 @@ describe('query', function () {
         const queryObject = find(userModel, {
             filter: { _id: '5938a0aefb6e41e0e8368d00' },
             lean: false,
+            limit: 20,
             projection: 'email username',
         });
 
@@ -24,6 +25,54 @@ describe('query', function () {
         expect(_conditions._id.toString()).to.equal('5938a0aefb6e41e0e8368d00');
         expect(_mongooseOptions.lean).to.equal(false);
         expect(_fields).to.have.all.keys('email', 'username');
+        expect(options.limit).to.equal(20);
+
+    });
+
+    it('will return a query object', function () {
+
+        const queryObject = find(userModel, {
+            filter: { _id: '5938a0aefb6e41e0e8368d00' },
+            lean: false,
+            projection: 'email username',
+        });
+
+        const { _conditions, _fields, _mongooseOptions, options } = queryObject;
+
+        expect(_conditions._id.toString()).to.equal('5938a0aefb6e41e0e8368d00');
+        expect(_mongooseOptions.lean).to.equal(false);
+        expect(_fields).to.have.all.keys('email', 'username');
+        expect(options.limit).to.equal(10);
+
+    });
+
+    it('will return a query object', function () {
+
+        const queryObject = find(userModel, {
+            filter: { _id: '5938a0aefb6e41e0e8368d00' },
+            projection: 'email username',
+        });
+
+        const { _conditions, _fields, _mongooseOptions, options } = queryObject;
+
+        expect(_conditions._id.toString()).to.equal('5938a0aefb6e41e0e8368d00');
+        expect(_mongooseOptions.lean).to.equal(true);
+        expect(_fields).to.have.all.keys('email', 'username');
+        expect(options.limit).to.equal(10);
+
+    });
+
+    it('will return a query object', function () {
+
+        const queryObject = find(userModel, {
+            filter: { _id: '5938a0aefb6e41e0e8368d00' },
+        });
+
+        const { _conditions, _fields, _mongooseOptions, options } = queryObject;
+
+        expect(_conditions._id.toString()).to.equal('5938a0aefb6e41e0e8368d00');
+        expect(_mongooseOptions.lean).to.equal(true);
+        expect(_fields).to.be.undefined;
         expect(options.limit).to.equal(10);
 
     });
@@ -41,6 +90,35 @@ describe('query', function () {
         expect(_conditions._id.toString()).to.equal('5938a0aefb6e41e0e8368d00');
         expect(_mongooseOptions.lean).to.equal(false);
         expect(_fields).to.have.all.keys('email', 'username');
+
+    });
+
+    it('will return a query object', function () {
+
+        const queryObject = findOne(userModel, {
+            filter: { _id: '5938a0aefb6e41e0e8368d00' },
+            projection: 'email username',
+        });
+
+        const { _conditions, _fields, _mongooseOptions } = queryObject;
+
+        expect(_conditions._id.toString()).to.equal('5938a0aefb6e41e0e8368d00');
+        expect(_mongooseOptions.lean).to.equal(true);
+        expect(_fields).to.have.all.keys('email', 'username');
+
+    });
+
+    it('will return a query object', function () {
+
+        const queryObject = findOne(userModel, {
+            filter: { _id: '5938a0aefb6e41e0e8368d00' },
+        });
+
+        const { _conditions, _fields, _mongooseOptions } = queryObject;
+
+        expect(_conditions._id.toString()).to.equal('5938a0aefb6e41e0e8368d00');
+        expect(_mongooseOptions.lean).to.equal(true);
+        expect(_fields).to.be.undefined;
 
     });
 
