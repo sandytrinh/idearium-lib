b66ed75c7512726b813cfc00593db62117456559
Scott Mebberson <scott@scottmebberson.com>
2017-06-15 10:48:06 +0930

    Minor changes regarding the Kue code.

--- idearium-lib/common/kue.js
+++ idearium-lib/common/kue.js
@@ -1,13 +1,6 @@
 'use strict';
 
-const log = require('./log')('idearium-lib:common:kue');
 const kueQueue = require('./kue-queue');
-const path = require('path');
-const { Loader } = require('../');
-
-// Setup the loader.
-const loader = new Loader();
-loader.camelCase = false;
 
 /**
  * Verify and create a factory method for creating kue jobs.
@@ -66,49 +59,4 @@ const constructCreateMethod = (type, properties) => {
 
 };
 
-/**
- * Load jobs from the jobs directory.
- * @return {Promise} Resolves with the jobs.
- */
-const loadJobs = () => loader.load(path.join(process.cwd(), 'jobs'));
-
-/**
- * Process all jobs in the jobs directory.
- * @return {Void} Processes the jobs.
- */
-const processJobs = () => new Promise((resolve, reject) => {
-
-    loadJobs()
-        .then((jobs) => {
-
-            const jobKeys = Object.keys(jobs);
-
-            // An array of job process functions.
-            const jobProcesses = jobKeys.map((type) => new Promise((resolve, reject) => {
-
-                kueQueue.process(type, (job, done) => {
-
-                    log.debug({ job, type }, `Processing job with type ${type}`);
-
-                    const fn = jobs[type];
-
-                    log.debug({ fn }, 'Calling function');
-
-                    fn(job)
-                        .then(data => resolve(done(null, data)))
-                        .catch(err => reject(done(err)));
-
-                });
-
-            }));
-
-            // Process the jobs.
-            return Promise.all(jobProcesses);
-
-        })
-        .then(resolve)
-        .catch(reject);
-
-});
-
-module.exports = { constructCreateMethod, processJobs };
+module.exports = { constructCreateMethod };
